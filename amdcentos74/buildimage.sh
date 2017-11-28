1a04c4218c0d1b59c0c16ca737297816d3436052
cvidler <cvidler@users.noreply.github.com>
2017-11-28 12:03:38 +1100

    establish CentOS 7.4 build environment. #10

---
+++ amdcentos74/buildimage.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+# Build ISO image from source 'disc' folder and add boot code for BIOS and UEFI.
+# Chris Vidler - Dynatrace DCRUM SME
+
+AMDVER="amd17"
+RHELVER="centos74"
+DISCTYPE="mini"
+VOLLABEL="CentOS 7 x86_64"
+
+OUTISO=`mktemp`
+#echo "OUTISO: [$OUTISO]"
+
+REV=""
+if [ -r revision.txt ]; then 
+    REV=`cat revision.txt` 
+fi
+if [ "$REV" == "" ]; then REV=1; fi
+#echo "REV: [$REV]"
+
+#implant version and disc label into postinstall
+VER="${AMDVER}_${RHELVER}_${DISCTYPE}${REV}"
+echo "[$VER]"
+EFI="disc/amd/efiamdrhel7min.cfg"
+BIOS="disc/amd/biosamdrhel7min.cfg"
+EFI1=`mktemp`
+BIOS1=`mktemp`
+VERFIND="echo \"AMD built with Dynatrace CentOS AMD Automated Installer.*"
+VERREPLACE="echo \"AMD built with Dynatrace CentOS AMD Automated Installer ${VER}\""
+cat "$EFI" | sed "s/${VERFIND}/${VERREPLACE}/" > "$EFI1"
+cat "$BIOS" | sed "s/${VERFIND}/${VERREPLACE}/" > "$BIOS1"
+
+mv -f "$EFI1" "$EFI"
+mv -f "$BIOS1" "$BIOS"
+rm -f "$EFI1" "$BIOS1"
+
+
+# build EFI image
+echo "1. Building new efiboot.img - sudo required, enter password when prompted"
+rm efiboot-new.img > /dev/null
+dd if=/dev/zero bs=1M count=10 of=efiboot-new.img
+mkfs.fat -n "ANACONDA" efiboot-new.img
+rm -rf newimage > /dev/null
+mkdir newimage
+sudo mount -o loop efiboot-new.img newimage
+sudo mkdir newimage/EFI 
+sudo cp -r disc/EFI/* newimage/EFI
+sudo umount newimage
+mv efiboot-new.img disc/images/efiboot.img
+rm -rf newimage
+
+echo "2. Building ISO image, ignore warnings"
+# build base ISO image
+cd disc
+mkisofs -U -input-charset utf-8 -volset "$VOLLABEL" -V "$VOLLABEL" -J -joliet-long -r \
+		-quiet -T -x ./lost+found -m TRANS.TBL -o $OUTISO \
+	    -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
+	    -boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot .
+ERROR=$?
+cd ..
+
+if [ ! $ERROR -eq 0 ]; then
+	rm -f $OUTISO
+	echo "mkisofs failed!"
+	exit 1
+fi
+
+# make is USB bootable
+echo "3. Adding USB boot support"
+isohybrid --uefi $OUTISO
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	rm -f $OUTISO
+	echo "isohybrid failed!"
+	exit 1
+fi
+
+# implant internal checksum
+echo "4. Implanting MD5 checksums"
+implantisomd5 --supported-iso  $OUTISO
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	rm -f $OUTISO
+	echo "implantisomd5 failed!"
+	exit 1
+fi
+
+
+echo "5. Creating ISO checksum"
+HASH=`md5sum $OUTISO`
+HASH=${HASH:0:32}
+#echo "HASH: [$HASH]"
+
+NEWISO="/tmp/${AMDVER}_${RHELVER}_${DISCTYPE}${REV}_${HASH}.iso"
+mv $OUTISO $NEWISO
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	echo "Could not rename ISO, ISO exists as: $OUTISO"
+	exit 1
+fi
+
+# git commit changes
+echo "6. Updating GIT"
+git commit --all --message "buildimage.sh run built image $NEWISO"
+
+
+REV=$((REV + 1))
+echo -E $REV > revision.txt
+echo
+echo "7. Created: $NEWISO"
+
---
+++ amdcentos74/updaterepo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# updates to repo data files to reflect added/removed packages
+
+echo -e "\e[34mINFO:\e[39m Preparing to update repo data."
+
+cd disc
+mv repodata/*-comps*.xml repodata/comps.xml
+createrepo -g repodata/comps.xml .
+if [ $? -ne 0 ]; then
+	echo -e "\e[33mWARNING:\e[39m Repo data update failed."
+else
+	echo -e "\e[32mPASS:\e[39m Repo data updated successfully."
+fi
+cd ..
+
