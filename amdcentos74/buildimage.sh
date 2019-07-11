dd43fe6b9956477e8359b7f84b1d06c8cafde6c2
cvidler <cvidler@users.noreply.github.com>
2019-07-11 15:32:01 +1000

    buildimage.sh run built image /tmp/probe18_centos76_mini18_dce908914a51a820995ab7cfe766307a.iso

--- amdcentos74/buildimage.sh
+++ amdcentos74/buildimage.sh
@@ -54,48 +54,110 @@ rm -f "$EFI1" "$BIOS1"
 
 # build EFI image
 techo "\e[34mINFO:\e[39m 1. Building new efiboot.img - sudo required, enter password when prompted"
-if [ -e efiboot-new.img ]; then rm efiboot-new.img > /dev/null ; fi
-dd if=/dev/zero bs=1M count=10 of=efiboot-new.img
-mkfs.fat -n "ANACONDA" efiboot-new.img
-rm -rf newimage > /dev/null
-mkdir newimage
-sudo mount -o loop efiboot-new.img newimage
-sudo mkdir newimage/EFI 
-sudo cp -r disc/EFI/* newimage/EFI
-sudo umount newimage
-mv efiboot-new.img disc/images/efiboot.img
-rm -rf newimage
 
-techo "\e[34mINFO:\e[39m 2. Building ISO image, ignore warnings"
-# build base ISO image
-cd disc
-mkisofs -U -input-charset utf-8 -volset "$VOLLABEL" -V "$VOLLABEL" -J -joliet-long -r \
-		-quiet -T -x ./lost+found -m TRANS.TBL -o $OUTISO \
-	    -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
-	    -boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot .
+if [ -e efiboot-new.img ]; then rm efiboot-new.img ; fi
+OUTPUT=$(dd if=/dev/zero bs=1M count=10 of=efiboot-new.img)
 ERROR=$?
-cd ..
+if [ ! $ERROR -eq 0 ]; then
+	techo "\e[31m***FATAL:\e[0m dd failed!"
+	techo $OUTPUT
+	quit $ERROR
+fi
 
+OUTPUT=$(mkfs.fat -n "ANACONDA" efiboot-new.img)
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	techo "\e[31m***FATAL:\e[0m mkfs.fat failed!"
+	techo $OUTPUT
+	quit $ERROR
+fi
+
+if [ -e newimage ]; then rm -rf newimage ; fi
+mkdir newimage
+ERROR=$?
 if [ ! $ERROR -eq 0 ]; then
 	techo "\e[31m***FATAL:\e[0m mkisofs failed!"
 	quit $ERROR
 fi
 
-# make is USB bootable
+
+sudo mount -o loop efiboot-new.img newimage
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	techo "\e[31m***FATAL:\e[0m mount efiboot failed!"
+	quit $ERROR
+fi
+
+sudo mkdir newimage/EFI 
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	techo "\e[31m***FATAL:\e[0m mkdir failed!"
+	quit $ERROR
+fi
+
+sudo cp -r disc/EFI/* newimage/EFI
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	techo "\e[31m***FATAL:\e[0m cp failed!"
+	quit $ERROR
+fi
+
+sudo umount newimage
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	techo "\e[31m***FATAL:\e[0m umount failed!"
+	quit $ERROR
+fi
+
+mv efiboot-new.img disc/images/efiboot.img
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	techo "\e[31m***FATAL:\e[0m mv failed!"
+	quit $ERROR
+fi
+
+rm -rf newimage
+ERROR=$?
+if [ ! $ERROR -eq 0 ]; then
+	techo "\e[31m***FATAL:\e[0m rm failed!"
+	quit $ERROR
+fi
+
+
+techo "\e[34mINFO:\e[39m 2. Building ISO image, ignore warnings"
+# build base ISO image
+cd disc
+OUTPUT=$(mkisofs -U -input-charset utf-8 -volset "$VOLLABEL" -V "$VOLLABEL" -J -joliet-long -r \
+		-quiet -T -x ./lost+found -m TRANS.TBL -o $OUTISO \
+	    -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 \
+	    -boot-info-table -eltorito-alt-boot -e images/efiboot.img -no-emul-boot .)
+ERROR=$?
+cd ..
+
+if [ ! $ERROR -eq 0 ]; then
+	techo "\e[31m***FATAL:\e[0m mkisofs failed!"
+	techo "$OUTPUT"
+	quit $ERROR
+fi
+
+
+# make it USB bootable
 techo "\e[34mINFO:\e[39m 3. Adding USB boot support"
-isohybrid --uefi $OUTISO
+OUTPUT=$(isohybrid --uefi $OUTISO)
 ERROR=$?
 if [ ! $ERROR -eq 0 ]; then
 	techo "\e[31m***FATAL:\e[0m isohybrid failed!"
+	techo "$OUTPUT"
 	quit $ERROR
 fi
 
 # implant internal checksum
 techo "\e[34mINFO:\e[39m 4. Implanting MD5 checksums"
-implantisomd5 --supported-iso  $OUTISO
+OUTPUT=$(implantisomd5 --supported-iso  $OUTISO)
 ERROR=$?
 if [ ! $ERROR -eq 0 ]; then
 	techo "\e[31m***FATAL:\e[0m implantisomd5 failed!"
+	techo "$OUTPUT"
 	quit $ERROR
 fi
 
